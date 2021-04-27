9f469b2e17b008a22f36f5504eb0b24831c885ea
pmc7758 <614039502@qq>
2021-04-27 23:22:53 +0800

    登录修改

--- src/main/java/jdbc/LoginJDBC.java
+++ src/main/java/jdbc/LoginJDBC.java
@@ -8,22 +8,22 @@ import java.sql.*;
 /**
  * @author pangjian
  * @ClassName LoginJDBC
- * @Description TODO
+ * @Description 查找用户账号和密码，认证通过则返回用户角色
  * @date 2021/4/24 10:45
  */
 
 public class LoginJDBC {
 
+    private static final String URL = "jdbc:oracle:thin:@47.119.128.150:1521:orcl";
+
     public static String login(String username,String password) throws ClassNotFoundException, SQLException {
 
-
-        String url = "jdbc:oracle:thin:@47.119.128.150:1521:orcl";
 
         String sql = "SELECT * FROM S_USER WHERE USERNAME='"+username+"'";
 
         Class.forName("oracle.jdbc.driver.OracleDriver");
 
-        Connection conn = DriverManager.getConnection(url,"scott","tiger");
+        Connection conn = DriverManager.getConnection(URL,"scott","tiger");
 
         Statement statement = conn.createStatement();
 
@@ -32,17 +32,17 @@ public class LoginJDBC {
         resultSet.next();
         String codePassword=resultSet.getString("password");
         String role = resultSet.getString("role");
-        String userPassword = MD5Utils.code(password);
+        String userPassword = MD5Utils.code(password);// 加密后对比
         if(userPassword.equals(codePassword)){
             resultSet.close();
             statement.close();
             conn.close();
-            return role;
+            return role; //返回用户角色
         }else {
             resultSet.close();
             statement.close();
             conn.close();
-            throw new RuntimeException("账号或密码错误!");
+            throw new RuntimeException("账号或密码错误!");//抛出异常
         }
 
     }
