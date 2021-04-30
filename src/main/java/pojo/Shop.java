30e7dd7b229084211d8009e8c356e18cff63309b
pmc7758 <614039502@qq>
2021-04-30 13:17:30 +0800

    商家实体类

---
+++ src/main/java/pojo/Shop.java
@@ -0,0 +1,39 @@
+package pojo;
+
+/**
+ * @author pangjian
+ * @ClassName Shop
+ * @Description 商店实体类
+ * @date 2021/4/30 10:11
+ */
+
+public class Shop {
+
+    private Integer id;
+    private String name;
+    private String area;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
